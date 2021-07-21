c9304e8b2f7af5cc612855012d28d945456429ef
gourav s <gourav.s@cisinlabs.com>
2021-07-21 13:39:06 +0530

    Changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,8 @@
-@if($remove_header)
+@if(isset($remove_header) && $remove_header === true)
 @include('layouts.header')
 @endif
 @yield('content')
-@if($remove_header)
+@if(isset($remove_header) && $remove_header === true)
  <!---footer start her -->
 @include('layouts.footer')
 @endif
