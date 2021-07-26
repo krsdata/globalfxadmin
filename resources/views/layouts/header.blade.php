946c27eea39f7605625426a99f4060ebb01d7b4a
gourav s <gourav.s@cisinlabs.com>
2021-07-26 14:30:12 +0530

    Changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,4 +16,13 @@
     <link rel=stylesheet href="{{ URL::asset('webmedia/css/style.css')}}">
     <link rel=stylesheet type=text/css href="{{ URL::asset('webmedia/css/layerslider.css')}}">
     <script src="{{ URL::asset('webmedia/js/vendor/modernizr.js')}}"></script>
+    
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KVN6NHG');</script>
+<!-- End Google Tag Manager -->
+
 </head>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.master') 
 
     @section('content') 
-<body><!--[if lt IE 10]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
-    your browser</a> to improve your experience.</p>
-<![endif]-->
+<body>
+    
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KVN6NHG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class=wrapper>
 @include('layouts.menu')
 
