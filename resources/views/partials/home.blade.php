7fd3091bc078a2b20c4049369287d88c6ab162cb
Your Name <you@example.com>
2021-08-18 19:52:39 +0530

    changes

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -12,6 +12,45 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 @include('layouts.menu')
 
 
+<section class="slider-up">
+
+<div class="container-fluid">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   <!--  <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol> -->
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="webmedia/images/banner/background01.jpg" alt="Los Angeles">
+      </div>
+
+      <div class="item">
+        <img src="webmedia/images/banner/background01.jpg" alt="Chicago">
+      </div>
+    
+      <div class="item">
+        <img src="webmedia/images/banner/background01.jpg" alt="New york">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</section>
+
 
 
     <div class=banner-text>
@@ -301,5 +340,6 @@ Payment Accepted by All Major Banks & Payment Wallets</p>
         </div>
     </section>
   
+
 
 
