4f22adabb6ddeadaac8060bc84ccf48ca44e7a03
Your Name <you@example.com>
2021-08-23 13:58:06 +0530

    changes

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -16,7 +16,7 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
                <div class="item active">
-                  <img src="webmedia/images/banner/background02.png" alt="Chicago">
+                  <img src="webmedia/images/banner/background02.jpg" alt="jk">
                   <div class="container">
                      <div class="carousel-caption">
                         <h1>DIL SE KHELO ON JUSTKHELO</h1>
@@ -26,7 +26,7 @@
                   </div>
                </div>
                <div class="item">
-                  <img src="webmedia/images/banner/bg-3.jpg" alt="New york">
+                  <img src="webmedia/images/banner/bg-3.jpg" alt="jk">
                   <div class="container">
                      <!-- <div class="carousel-caption">
                         <h1>Join Now</h1>
@@ -36,7 +36,7 @@
                   </div>
                </div>
                <div class="item">
-                  <img src="webmedia/images/banner/background-02.jpg" alt="New york">
+                  <img src="webmedia/images/banner/background-02.jpg" alt="jk">
                   <div class="container">
                      <div class="carousel-caption">
                         <!-- <h1>Download Now </h1>
@@ -68,7 +68,7 @@
       <div class="container">
          <div class="col-md-12 text-center">
             <h2 class="heading line">JUSTKHELO<span> FANTASY SPORTS</span></h2>
-            <p class="text justify"> We drive one of the biggest virtual yet fancy sports platform. Also, we help you set-up your fan base by keeping a watch over shared posts in the feed. Not just this, but here you can enhance your performance by playing more to reach the next best level along with exciting cash rewards. Here you can create a team choosing your favourite players which help you gain more coins in any contest. JUSTKHELO Fantasy League is a stage that permits you to play virtually opting amongst real-life players and earn points using your game expertise and knowledge </p>
+            <p class="text justify"> We drive one of the biggest virtual yet fancy sports platform. Also, we help you set-up your fan base by keeping a watch over shared posts in the feed. Not just this, but here you can enhance your performance by playing more to reach the next best level along with exciting cash rewards. Here you can create a team choosing your favourite players which help you gain more coins in any contest. JUSTKHELO Fantasy League is a stage that permits you to play virtually opting amongst real-life players and earn points using your game expertise and knowledge. </p>
             <div class="col-md-12 text-center download">
                <a href="https://justkhelo.com/download-app?_branch_name=001">
                <button type="button" class="btn btn-info">Download Now</button>
@@ -91,7 +91,7 @@
                   </div>
                   <div class="info">
                      <h5 class="text-center">Check Out Contest For the League</h5>
-                     <p>You are allowed to check over the participating teams based on the previous match listings, and you can also check the entry ticket amount</p>
+                     <p>You are allowed to check over the participating teams based on the previous match listings, and you can also check the entry ticket amount.</p>
                   </div>
                </div>
             </div>
@@ -102,7 +102,7 @@
                   </div>
                   <div class="info">
                      <h5 class="text-center">Create your Best Team</h5>
-                     <p>JUSTKHELO gives you an opportunity to organize the best team, choosing from real-life players and get paid for your knowledge & expertise by winning the cash rewards</p>
+                     <p>JUSTKHELO gives you an opportunity to organize the best team, choosing from real-life players and get paid for your knowledge & expertise by winning the cash rewards.</p>
                   </div>
                </div>
             </div>
@@ -131,12 +131,12 @@
                <div class="col-md-4 spacing">
                   <div class="img_space"><img src="{{URL::asset('webmedia/images/icons/1Quickest.png')}}" alt=image class="img-thumbnail" width="70"></div>
                   <h4>India’s Quickest Fantasy Game</h4>
-                  <p>Create your teams with 2/3/4 Players to enjoy Fantasy Gaming</p>
+                  <p>Create your teams with 2/3/4 Players to enjoy Fantasy Gaming.</p>
                </div>
                <div class="col-md-4 spacing">
                   <div class="img_space"><img src="{{URL::asset('webmedia/images/icons/2bonus.png')}}" alt=image class="img-thumbnail" width="70"></div>
                   <h4>Play 100% Bonus and Free Entry Contests</h4>
-                  <p>Play Free Entry Contests and use 100% of your sign-up bonus to join contests</p>
+                  <p>Play Free Entry Contests and use 100% of your sign-up bonus to join contests.</p>
                </div>
                <div class="col-md-4 spacing">
                   <div class="img_space"><img src="{{URL::asset('webmedia/images/icons/3winning.png')}}" alt=image class="img-thumbnail" width="70"></div>
@@ -147,17 +147,17 @@
             <div class="col-md-4 spacing">
                <div class="img_space"><img src="{{URL::asset('webmedia/images/icons/4instantbonus.png')}}" alt=image class="img-thumbnail" width="70"></div>
                <h4>Instant INR 100 Welcome Bonus</h4>
-               <p>Get 100% welcome bonus up to INR 5000 on your First Deposit</p>
+               <p>Get 100% welcome bonus up to INR 5000 on your First Deposit.</p>
             </div>
             <div class="col-md-4 spacing">
                <div class="img_space"><img src="{{URL::asset('webmedia/images/icons/5creditscore.png')}}" alt=image class="img-thumbnail" width="70"></div>
                <h4>Lowest Credit Scores</h4>
-               <p>In our 11-Player game, you will find the lowest Credit Scores for all the players</p>
+               <p>In our 11-Player game, you will find the lowest Credit Scores for all the players.</p>
             </div>
             <div class="col-md-4 spacing">
                <div class="img_space"><img src="{{URL::asset('webmedia/images/icons/6support.png')}}" alt=image class="img-thumbnail" width="70"></div>
                <h4>Best Customer Support</h4>
-               <p>Our dedicated Support Team answers your KYC or Withdrawal queries across email and social handles</p>
+               <p>Our dedicated Support Team answers your KYC or Withdrawal queries across email and social handles.</p>
             </div>
          </div>
       </div>
