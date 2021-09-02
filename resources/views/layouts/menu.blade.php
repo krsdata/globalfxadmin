b0a225c3aefcbadf1127fd1360086f25c2d5bb3d
Your Name <you@example.com>
2021-09-02 20:21:47 +0530

    changes

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -15,33 +15,12 @@
             <ul class="nav navbar-nav navbar-right">
                <li><a href="{{url('/')}}">Home</a></li>
                <li><a  href="{{url('how-to-play')}}">How To Play</a></li>
-                <li><a  href="{{url('how-to-play')}}">How To Install</a></li>
+                <li><a  href="https://justkhelo.com/download-app?_branch_name=001">How To Install</a></li>
                 <li><a href="{{url('/about-us')}}">About</a></li>
                 <li><a  href="{{url('contact-us')}}">Help</a></li>
 
 
-               <!-- social icon-->
-               
-          <!--         <ul class="social_icon navbar-right">
-                  <li class="nav-item">
-                     <a href="{{$settings->facebook_url}}" class="social-link">
-            <img src="webmedia/images/banner/fb.svg" class="img-thumbnail" alt="social" width="40">
-                     </a>
-                  </li>
-                  <li class="nav-item">
-                     <a href="{{$settings->instagram_url}}" class="social-link">
-      <img src="webmedia/images/banner/insta.svg" class="img-thumbnail" alt="social" width="40" >
-                     </a>
-                  </li>
-                  <li class="nav-item"><a href="{{$settings->twitter_url}}" class="social-link">
-   <img src="webmedia\images\banner/twitter.svg" class="img-thumbnail " alt="social" width="40">
-                     </a>
-                  </li>
-                  <li class="nav-item"><a href="{{$settings->youtube_url}}" class="social-link">
-   <img src="webmedia\images\banner/youtube.svg" class="img-thumbnail" alt="social" width="40">
-                     </a>
-                  </li>
-               </ul> -->
+              
              </ul>
             
          </div>
