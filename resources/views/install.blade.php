e0db629cf2d36ad3755298c640c523622c04e2cc
gourav s <gourav.s@cisinlabs.com>
2021-09-14 16:40:31 +0530

    Changes

--- resources/views/install.blade.php
+++ resources/views/install.blade.php
@@ -12,13 +12,31 @@
       @endphp 
       <div class="innerbannerinstall">
         
-         <h2 class="bannerHeadline">
-           <a href="{{$url}}" style="color:#1041b7; display: block; padding:10px; font-weight: 600;">Download Play & Win</a> </h2>
-          
+        
         
       </div>
    </div>
-
+   <div class=banner-text-install>
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12 text-center download">
+               <a href="{{$url}}" class="btn btn-default" role="button">Download Now</a>
+            </div>
+            <div class="col-md-12">
+               Download Now and Win UpTo 1CR Today.
+            </div>
+         </div>
+      </div>
+   </div>
+   <section class="latest_news bg-white">
+      <div class="container">
+         <div class="row">
+            <h2 class="heading line">Our Rating Says,<span> You Love Us</span></h2>
+            <div class="secure_safe"> <img src="{{URL::asset('webmedia/images/rating.jpg')}}" alt=image></div>
+            <div class="secure_safe"><img src="{{URL::asset('webmedia/images/widget/safe_secure.jpg')}}" alt=image></div>
+         </div>
+      </div>
+   </section>
 <section class="wining_slider">
 <div class="container">
          <div class="row">
@@ -126,72 +144,14 @@
                         </div>
                      </li>
                   </ul>
-                  <div class="secure_safe"><img src="{{URL::asset('webmedia/images/widget/safe_secure.jpg')}}" alt=image></div>
+                
                </div>
             </div>
          </div>
       </div>
    </section>
    
-   <section class="latest_news bg-white">
-      <div class="container">
-         <div class="row">
-            <h2 class="heading line">Our Rating Says,<span> You Love Us</span></h2>
-            <div class="LatestNews_wrap clearfix">
-               <div class="tab-content news_display_container clearfix">
-                  <!-- <a class="prv club_prev"></a> <a class="nxt club_next"></a> -->
-                  <ul id=club_news class="tab-pane active clearfix slideHeroes">
-                     <li>
-                        <div class=figure>
-                           <div class=column-news>
-                              <div class=figure-01>
-                                 <div class="bg-black01 fig02">
-                     <img src="{{URL::asset('webmedia/images/testimonial/himani.png')}}" style="width:50px">
-                                    <h6 class="paragraph02">Himani Shankarv</h6>
-                                    <p class="uppercaseheading red">Mumbai</p>
-                                    <p>JustKhelo is a very easy platform to create teams and play. I have tried other fantasy games but to my disappointment they were hard to understand. I have also actually won on this app and OH BOY! I LOVE IT!!</p>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </li>
-                     <li>
-                        <div class=figure>
-                           <div class=column-news>
-                              <div class=figure-01>
-                                 <div class="bg-black01 fig02">
-                        <img src="{{URL::asset('webmedia/images/testimonial/mohit.png')}}" style="width:50px">
-                                    <h6 class="paragraph02">Mohit J</h6>
-                                    <p class="uppercaseheading red">Delhi</p>
-                                    <p>During lockdown being busy was a task. At home all day and nothing to do. JustKhelo came as a blessing. Selecting players and playing was fun and easy. When i got my first payout, I was over the moon! Play Hard and Earn Well was a reality. 
-                                    </p>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </li>
-                     <li>
-                        <div class=figure>
-                           <div class=column-news>
-                              <div class=figure-01>
-                                 <div class="bg-black01 fig02">
-                        <img src="{{URL::asset('webmedia/images/testimonial/kapil.png')}}" style="width:50px">
-                                    <h6 class="paragraph02">Kapil D</h6>
-                                    <p class="uppercaseheading red">Jaipur</p>
-                                    <p>If you play on JustKhelo you will forget other game apps. It is so engaging and the process of payout, customer service is seamless. It is a MUST TRY APP!
-                                    </p>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </li>
-                  </ul>
-               </div>
-               <div class="secure_safe"> <img src="{{URL::asset('webmedia/images/rating.jpg')}}" alt=image></div>
-            </div>
-         </div>
-      </div>
-   </section>
+  
    <section class="latest_news bg-white">
       <div class="container">
          <div class="row">
